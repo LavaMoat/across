873d01371fbde046a5ca445bea585584d012bd58
weizman <weizmangal@gmail.com>
2022-07-18 03:33:46 +0300

    fix urls

--- demo/receiver.js
+++ demo/receiver.js
@@ -2,6 +2,8 @@
     document.onmessage((src, msg) => {
         if (src === 'https://weizman.github.io/across/demo/sender.js') {
             alert(`Got a trusted message from "${src}": \n\n` + msg);
+        } else {
+            alert(`Got a fake message from "${src}": \n\n` + msg + '\n\nNICE TRY!');
         }
     })
 }())
