34c954471f18cf9b8b1db233c6d67fd616bcaa69
weizman <weizmangal@gmail.com>
2022-12-28 11:35:21 +0200

    add delay to done

--- test/scripts/main.js
+++ test/scripts/main.js
@@ -1,6 +1,8 @@
 (function(){
     const postMessage = document.onmessage((src, msg) => {
-        done('GOT MESSAGE FROM SCRIPT "' + getName(src) + '" : ' + msg);
+        setTimeout(() => {
+            done('GOT MESSAGE FROM SCRIPT "' + getName(src) + '" : ' + msg);
+        }, 30);
     });
     setTimeout(() => {
         postMessage(getSrc('legit'), 'PING');
